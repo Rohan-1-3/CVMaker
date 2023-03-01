5b3dff16696f1bc9940dfd278284fe6771db5b78
Rohan-1-3 <rohanshrestha130@gmail.com>
2023-03-01 14:03:51 +0545

    added href

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import InitialDesign from "./components/InitialDesign";
 function App() {
   return (
     <div className="App">
-      <InitialDesign title = "E-G CV" href=""/>
+      <InitialDesign title = "E-G CV" href="https://github.com/Rohan-1-3/CVMaker"/>
     </div>
   );
 }
